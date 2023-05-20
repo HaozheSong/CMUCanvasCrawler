d4e3d686285ef643a2d3e907878bd569b5d72ddb
Sora <sora@sorapage.com>
2023-05-19 22:49:20 -0400

    crawler-0.2.0 Explicit wait

--- crawler.py
+++ crawler.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from cookie import COOKIE
 
 
@@ -10,7 +12,7 @@ class Crawler:
         installation = ChromeDriverManager(path=web_driver_path).install()
         service = ChromeService(installation)
         self.driver = webdriver.Chrome(service=service)
-        self.driver.implicitly_wait(wait_second)
+        self.wait_second = wait_second
 
     def close(self):
         self.driver.quit()
@@ -22,18 +24,30 @@ class Crawler:
     def crawl(self, url, course_cnt):
         driver = self.driver
         driver.get(url)
-        driver.switch_to.frame("tool_content")
+        self.switch_to_frame("tool_content")
         crawled_cnt = 0
         while crawled_cnt < course_cnt:
-            table = driver.find_element(By.ID, "detailsTable")
+            table = self.find_element(By.ID, "detailsTable")
             courses = table.find_elements(By.CLASS_NAME, "detail-cell")
             crawled_cnt += len(courses)
             print(crawled_cnt)
             self.click_next()
 
     def click_next(self):
-        next_btn = self.driver.find_element(By.ID, "nextPage")
+        next_btn = self.find_element(By.ID, "nextPage")
         next_btn.click()
+
+    def find_element(self, by, value):
+        element = WebDriverWait(self.driver, self.wait_second).until(
+            EC.presence_of_element_located((by, value))
+        )
+        return element
+
+    def switch_to_frame(self, frame_id):
+        element = WebDriverWait(self.driver, self.wait_second).until(
+            EC.frame_to_be_available_and_switch_to_it(frame_id)
+        )
+        return element
 
 
 crawler = Crawler()
