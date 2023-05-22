71f394091b270a92318d63f23560484107a4fd49
Sora <sora@sorapage.com>
2023-05-22 14:49:45 -0400

    crawler-0.4.0 Fluent wait & crawl recording video pages

--- crawler.py
+++ crawler.py
@@ -1,3 +1,4 @@
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -13,6 +14,7 @@ class Crawler:
         service = ChromeService(installation)
         self.driver = webdriver.Chrome(service=service)
         self.wait_second = wait_second
+        self.wait = WebDriverWait(self.driver, timeout=self.wait_second)
 
     def close(self):
         self.driver.quit()
@@ -21,40 +23,83 @@ class Crawler:
         self.driver.get("https://canvas.cmu.edu/404")
         self.driver.add_cookie(cookie)
 
-    def crawl(self, url, course_cnt):
+    def crawl_recordings(self, url):
+        recordings = []
         driver = self.driver
         driver.get(url)
         self.switch_to_frame("tool_content")
+        total = self.parse_page_range()["total"]
         crawled_cnt = 0
-        while crawled_cnt < course_cnt:
-            table = self.find_element(By.ID, "detailsTable")
-            courses = table.find_elements(By.CLASS_NAME, "detail-cell")
-            crawled_cnt += len(courses)
-            print(crawled_cnt)
-            if crawled_cnt < course_cnt:
-                self.click_next(courses[0])
+        while crawled_cnt < total:
+            self.parse_page_range()  # wait until the page is fully loaded
+            cells_e = self.find_elements(By.CLASS_NAME, "detail-cell")
+            for e in cells_e:
+                description_e = e.find_element(By.CLASS_NAME, "item-description")
+                r_description = description_e.text
+                detail_title_a_e = e.find_element(By.CSS_SELECTOR, "a.detail-title")
+                r_link = detail_title_a_e.get_attribute("href")
+                detail_title_span_e = detail_title_a_e.find_element(By.TAG_NAME, "span")
+                r_title = detail_title_span_e.text
+                if r_title == "" and r_link is None and r_description == "":
+                    continue
+                crawled_cnt += 1
+                r = Recording(crawled_cnt, r_title, r_link, r_description)
+                recordings.append(r)
+                print(r)
+            if crawled_cnt < total:
+                self.click_next(cells_e[0])
+        return recordings
 
     def click_next(self, element):
-        next_btn = self.find_element(By.ID, "nextPage")
-        next_btn.click()
+        next_btn_e = self.find_element(By.ID, "nextPage")
+        next_btn_e.click()
         WebDriverWait(self.driver, self.wait_second).until(
             EC.staleness_of(element)
         )
 
     def find_element(self, by, value):
-        element = WebDriverWait(self.driver, self.wait_second).until(
-            EC.presence_of_element_located((by, value))
-        )
+        element = self.wait.until(EC.presence_of_element_located((by, value)))
         return element
 
+    def find_elements(self, by, value):
+        elements = self.wait.until(EC.presence_of_all_elements_located((by, value)))
+        return elements
+
     def switch_to_frame(self, frame_id):
-        element = WebDriverWait(self.driver, self.wait_second).until(
-            EC.frame_to_be_available_and_switch_to_it(frame_id)
-        )
-        return element
+        frame = self.wait.until(EC.frame_to_be_available_and_switch_to_it(frame_id))
+        return frame
+
+    def parse_page_range(self):
+        self.wait.until(EC.text_to_be_present_in_element((By.ID, "pageRange"), "Viewing"))
+        e = self.find_element(By.ID, "pageRange")
+        range_str = e.text
+        pattern = re.compile(r'\d+')
+        [start_index, end_index, total] = pattern.findall(range_str)
+        recordings_this_page = int(end_index) - int(start_index) + 1
+        page_range = {
+            "start": int(start_index),
+            "end": int(end_index),
+            "recordings_this_page": recordings_this_page,
+            "total": int(total)
+        }
+        return page_range
+
+
+class Recording:
+    def __init__(self, index, title, link, description):
+        self.index = index
+        self.title = title
+        self.link = link
+        self.description = description
+
+    def __str__(self):
+        s = (f"[{self.index}] {self.title}\n"
+             f"{self.description}\n"
+             f"{self.link}\n")
+        return s
 
 
 crawler = Crawler()
 crawler.login_by_cookie(COOKIE)
-crawler.crawl("https://canvas.cmu.edu/courses/33119/external_tools/467", 32)
+crawler.crawl_recordings("https://canvas.cmu.edu/courses/33119/external_tools/467")
 crawler.close()
